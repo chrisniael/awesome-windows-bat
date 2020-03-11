2e2f8ecbdea6bbc77b737cd21b179ec40465a594
shenyu <shenyu@shenyu.me>
2020-03-11 12:00:37 +0800

    Update stop-screen-save.bat

--- stop-screen-save.bat
+++ stop-screen-save.bat
@@ -1,8 +1,23 @@
-:: 取消屏幕保护和屏幕保护解锁密码
-
 @echo off
 
+Rem 使用管理员身份运行
+cd /d "%~dp0"
+cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
+if %errorlevel%==0 goto Admin
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
+echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
+echo WScript.Quit >>"%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs" /f
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+exit
+
+:Admin
+
+Rem 关闭屏幕保护
 reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v ScreenSaveActive /t REG_SZ /d "0" /f
+
+Rem 唤醒时关闭密码保护
 reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v ScreenSaverIsSecure /t REG_SZ /d "0" /f
 
 pause
