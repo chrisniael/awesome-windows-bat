469bd63744d7c1e2f0d84f3c42c2eebbfebcce72
shenyu <shenyu@shenyu.me>
2020-03-26 17:53:21 +0800

    Remove admin in stop-screen-save.bat

--- stop-screen-save.bat
+++ stop-screen-save.bat
@@ -1,19 +1,5 @@
 @echo off
 
-Rem 使用管理员身份运行
-cd /d "%~dp0"
-cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
-if %errorlevel%==0 goto Admin
-if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
-echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
-echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
-echo WScript.Quit >>"%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs" /f
-if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
-exit
-
-:Admin
-
 Rem 关闭屏幕保护
 reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v ScreenSaveActive /t REG_SZ /d "0" /f
 
